f7162753ffd44e528642b3dbff34b5f0914e2ccd
toanbui626391 <toanbui626391@gmail.com>
2024-07-19 13:46:48 +0700

    update

---
+++ 3_chains/2_chains_under_the_hood.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableSequence
+from langchain_openai import ChatOpenAI
+
+"""
+Example about RnnableLambda, RunableSequence. Which is how pipe run under the hood
+    RunnableLambda: is a object which is just a wrap over of python lambda function
+    RunnableSequence: object which define how to chain RunnableLamda together
+"""
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatOpenAI(model="gpt-4")
+
+# Define prompt templates
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+)
+
+# Create individual runnables (steps in the chain)
+format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
+invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
+parse_output = RunnableLambda(lambda x: x.content)
+
+# Create the RunnableSequence (equivalent to the LCEL chain)
+chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
+
+# Run the chain
+response = chain.invoke({"topic": "lawyers", "joke_count": 3})
+
+# Output
+print(response)
